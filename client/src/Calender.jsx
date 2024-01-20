2b947ad4efb94a080f99ee53cada5b0efed14ef5
bajisapp@gmail.com <bajisapp@gmail.com>
2024-01-20 09:18:58 +0800

    first tesst  commit

--- client/src/Calender.jsx
+++ client/src/Calender.jsx
@@ -6,7 +6,6 @@ import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './time.css';
-import './App.css';
 
 
 
